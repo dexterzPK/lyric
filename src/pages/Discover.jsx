38e440bce9c3a05f99cdaad11b65d5f79b193a99
dexterzPK <134509031+dexterzPK@users.noreply.github.com>
2023-05-31 17:32:40 +0530

    all for now

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,7 +1,10 @@
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 const Discover = () => {
+  const { data, isFetching, error } = useGetTopChartsQuery();
+  console.log(data);
   const genreTitle = "Pop";
   return (
     <div className="flex flex-col">
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,18 +1,21 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "d1dce91ad8msh7a0fd701ec5abb9p13fb98jsn7ba7c942fbfc",
-    "X-RapidAPI-Host": "shazam-core.p.rapidapi.com",
-  },
-};
-
-fetch("https://echo.paw.cloud/", options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
-
 export const shazamCoreApi = createApi({
   reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1/",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "d1dce91ad8msh7a0fd701ec5abb9p13fb98jsn7ba7c942fbfc"
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world " }),
+  }),
 });
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
